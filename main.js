28f2ad9b34eae3bcc10bc25117e4ee12a29a26ed
Konstantin Gulyy <kgulyy@my.com>
2020-02-15 16:46:17 +0300

    Pop up implement

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const app = document.getElementById('app');
+app.className = 'main';
+
+const button = document.createElement('button');
+button.textContent = 'Show  magic';
+
+const popUp = document.createElement('div');
+popUp.className = 'popup';
+popUp.hidden = true;
+popUp.textContent = 'Hello JS!';
+
+app.appendChild(button);
+app.appendChild(popUp);
+
+button.addEventListener('click', function () {
+    popUp.hidden = false;
+});
+
+popUp.addEventListener('click', function () {
+    popUp.hidden = false;
+});
+
+app.addEventListener('click', function () {
+    popUp.hidden = true;
+}, true);
